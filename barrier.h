036b97d307117e185c5f4324041eed204d012adb
Abdur-Fadill <lepsipepi42@gmail.com>
2025-03-02 22:54:53 +0700

    summary

--- barrier.h
+++ barrier.h
@@ -1,9 +1,14 @@
 #ifndef BARRIER_H
 #define BARRIER_H
 
+#include<graphics.h>
+
+void initializeScreen();    
 int calculateBarrierStartX();
 void drawBarrier(int index, int x, int y);
 void drawBullet(Bullet bullet);
 void checkBulletCollision(Bullet *bullet, int startX, int barrierY);
 void drawAllBarriers(int startX, int barrierY);
 void initializeBarrierState();
+
+#endif

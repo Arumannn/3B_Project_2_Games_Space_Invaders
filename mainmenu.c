d90eafded1c0a4454e6f663261bf2a7d29ab3eba
Arumannn <izli43iz@gmail.com>
2025-03-03 08:05:40 +0700

    commit

--- mainmenu.c
+++ mainmenu.c
@@ -1,57 +1,111 @@
 #include <graphics.h>
+#include <conio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "mainmenu.h"
 
-// Fungsi untuk inisialisasi graphics
-void initGraphics() {
+// Fungsi untuk menggambar teks di tengah koordinat tertentu
+void drawText(int x, int y, const char* text, int size, int color) {
+    setcolor(color);
+    settextstyle(DEFAULT_FONT, HORIZ_DIR, size);
+    settextjustify(CENTER_TEXT, CENTER_TEXT);
+
+    // Salin string ke buffer lokal untuk menghindari error tipe data
+    char tempText[100];
+    strncpy(tempText, text, sizeof(tempText) - 1);
+    tempText[sizeof(tempText) - 1] = '\0'; // Pastikan null-terminated
+
+    // Tampilkan teks di koordinat tengah
+    outtextxy(x, y, tempText);
+}
+
+// Fungsi untuk menggambar alien sederhana
+void drawAlien(int x, int y, int size, int color) {
+    setcolor(color);
+    
+    // Kepala alien
+    rectangle(x, y, x + size, y + size);
+    floodfill(x + size / 2, y + size / 2, color);
+
+    // Mata
+    setcolor(BLACK);
+    rectangle(x + size / 4, y + size / 3, x + size / 4 + size / 5, y + size / 3 + size / 5);
+    rectangle(x + size - size / 4 - size / 5, y + size / 3, x + size - size / 4, y + size / 3 + size / 5);
+    floodfill(x + size / 4 + 1, y + size / 3 + 1, BLACK);
+    floodfill(x + size - size / 4, y + size / 3 + 1, BLACK);
+
+    // Kaki
+    setcolor(color);
+    rectangle(x + size / 4, y + size, x + size / 4 + size / 5, y + size + size / 4);
+    rectangle(x + size - size / 4 - size / 5, y + size, x + size - size / 4, y + size + size / 4);
+    floodfill(x + size / 4 + 1, y + size + 1, color);
+    floodfill(x + size - size / 4, y + size + 1, color);
+}
+
+// Fungsi untuk menampilkan menu utama dalam mode fullscreen
+void showMainMenu() {
     int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");  // Path tidak diperlukan di sebagian besar compiler modern
+    initwindow(getmaxwidth(), getmaxheight(), "Space Invaders");
+
+    if (graphresult() != grOk) {
+        printf("Graphics initialization failed\n");
+        exit(1);
+    }
+
+    // Set latar belakang
     setbkcolor(BLACK);
     cleardevice();
-}
 
-// Fungsi untuk menggambar tombol
-void drawButton(int x, int y, int width, int height, const char *text) {
-    setcolor(WHITE);
-    rectangle(x, y, x + width, y + height);
-    setfillstyle(SOLID_FILL, LIGHTGRAY);
-    floodfill(x + 1, y + 1, WHITE);
+    int screenWidth = getmaxx();
+    int screenHeight = getmaxy();
 
-    // Menampilkan teks di tengah tombol
-    setbkcolor(LIGHTGRAY);
-    setcolor(BLACK);
-    settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 2);
+    // Tampilkan judul "SPACE INVADERS" besar
+    drawText(screenWidth / 2, screenHeight / 6, "SPACE INVADERS", 6, GREEN);
 
-    // Gunakan array untuk kompatibilitas
-    char textArray[100];
-    strncpy(textArray, text, sizeof(textArray) - 1);
-    textArray[sizeof(textArray) - 1] = '\0';
+    // Buat tombol "Play" yang lebih besar dan tengah
+    int btn_width = screenWidth / 5;
+    int btn_height = screenHeight / 10;
+    int btn_x = (screenWidth - btn_width) / 2;
+    int btn_y = screenHeight / 3;
+    
+    setcolor(GREEN);
+    rectangle(btn_x, btn_y, btn_x + btn_width, btn_y + btn_height);
+    setfillstyle(SOLID_FILL, GREEN);
+    floodfill(btn_x + 1, btn_y + 1, GREEN);
 
-    // Dapatkan ukuran teks
-    int textWidth = textwidth(textArray);
-    int textHeight = textheight(textArray);
+    setbkcolor(GREEN);
+    drawText(btn_x + btn_width / 2, btn_y + btn_height / 2, "Play", 4, BLACK);
 
-    // Tempatkan teks di tengah tombol
-    outtextxy(x + (width - textWidth) / 2, y + (height - textHeight) / 2, textArray);
-}
+    // Gambar 3 alien secara simetris di bawah tombol Play
+    int alien_size = screenWidth / 12;
+    int alien_y = btn_y + btn_height + screenHeight / 10;
 
-// Fungsi untuk menggambar judul
-void drawTitle() {
-    setcolor(WHITE);
-    settextstyle(BOLD_FONT, HORIZ_DIR, 5);
-    outtextxy(200, 50, "SPACE INVADERS");
-}
+    drawAlien(screenWidth / 3 - alien_size / 2, alien_y, alien_size, WHITE);
+    drawAlien(screenWidth / 2 - alien_size / 2, alien_y, alien_size, WHITE);
+    drawAlien(2 * screenWidth / 3 - alien_size / 2, alien_y, alien_size, WHITE);
 
-// Fungsi untuk menampilkan menu utama
-void showMainMenu() {
-    cleardevice();
-    drawTitle();
+    // Loop untuk menangkap input pengguna
+    int isRunning = 1;
+    while (isRunning) {
+        if (kbhit()) {
+            char key = getch();
+            if (key == 27) { // ESC untuk keluar
+                isRunning = 0;
+            }
+        }
 
-    // Menampilkan tombol
-    drawButton(250, 150, 200, 50, "Play");
-    drawButton(250, 220, 200, 50, "Instructions");
-    drawButton(250, 290, 200, 50, "Credits");
-
-    getch();  // Menunggu input sebelum menutup
+        // Cek jika tombol "Play" diklik
+        if (ismouseclick(WM_LBUTTONDOWN)) {
+            int mx = mousex();
+            int my = mousey();
+            clearmouseclick(WM_LBUTTONDOWN);
+    
+            if (mx >= btn_x && mx <= btn_x + btn_width &&
+                my >= btn_y && my <= btn_y + btn_height) {
+                cleardevice();
+                drawText(screenWidth / 2, screenHeight / 2, "Game Started...", 4, WHITE);
+                delay(2000);
+                isRunning = 0;
+            }
+        }
+    }
 }
--- mastermain.c
+++ mastermain.c
@@ -3,49 +3,49 @@
 #include <windows.h>
 #include "mainsprite.h"
 #include "mainmenu.h"
-//#include "alien.h"
+#include "alien.h"
 
 int main() {
     showMainMenu();
-//     Player SpaceShip_P = {getmaxx() / 2, getmaxy() / 2};
+    Player SpaceShip_P = {getmaxx() / 2, getmaxy() / 2};
 
-//     // Inisialisasi aliens
-//     Alien aliens[MAX_ALIENS];
-//     int alienDir = 1;
-//     initAliens(aliens);
+    // Inisialisasi aliens
+    Alien aliens[MAX_ALIENS];
+    int alienDir = 1;
+    initAliens(aliens);
 
-//     // Inisialisasi peluru
-//     initBullets();
+    // Inisialisasi peluru
+    initBullets();
 
-//     // Buffer untuk double buffering
-//     int buffer = imagesize(0, 0, getmaxx(), getmaxy());
-//     void *frameBuffer = malloc(buffer);
+    // Buffer untuk double buffering
+    int buffer = imagesize(0, 0, getmaxx(), getmaxy());
+    void *frameBuffer = malloc(buffer);
 
-//     int gameOver = 0;
-//     while (!gameOver) {
-//         getimage(0, 0, getmaxx(), getmaxy(), frameBuffer);
-//         if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
-//             break;
-//         }
+    int gameOver = 0;
+    while (!gameOver) {
+        getimage(0, 0, getmaxx(), getmaxy(), frameBuffer);
+        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+            break;
+        }
 
-//         // Cek kalau aliens nyampe bawah (game over)
-//         for (int i = 0; i < MAX_ALIENS; i++) {
-//             if (aliens[i].active && aliens[i].y >= getmaxy() - BLOCK_SIZE) {
-//                 gameOver = 1;
-//             }
-//         }
+        // Cek kalau aliens nyampe bawah (game over)
+        for (int i = 0; i < MAX_ALIENS; i++) {
+            if (aliens[i].active && aliens[i].y >= getmaxy() - BLOCK_SIZE) {
+                gameOver = 1;
+            }
+        }
 
-//         cleardevice();
-//         SpaceshipMove(&SpaceShip_P);
-//         updateBullets();
-//         updateAliens(aliens, &alienDir);
-//         DrawSpaceShip(&SpaceShip_P); // Ubah jadi DrawSpaceShip
-//         drawBullets();
-//         drawAliens(aliens);
-//         delay(30);
-//     }
+        cleardevice();
+        SpaceshipMove(&SpaceShip_P);
+        updateBullets();
+        updateAliens(aliens, &alienDir);
+        DrawSpaceShip(&SpaceShip_P); // Ubah jadi DrawSpaceShip
+        drawBullets();
+        drawAliens(aliens);
+        delay(30);
+    }
 
-//     free(frameBuffer);
-     closegraph(); // WinBGIm biasanya ga perlu parameter
-     return 0;
- }
+    free(frameBuffer);
+    closegraph(); // WinBGIm biasanya ga perlu parameter
+    return 0;
+}

91766fac8feeeff882c863816b165d7f63560dd9
Abdur-Fadill <lepsipepi42@gmail.com>
2025-03-13 13:04:26 +0700

    commitsatu

--- barrier.c
+++ barrier.c
@@ -7,17 +7,20 @@ void drawBarrier(int x, int y) {
     setfillstyle(SOLID_FILL, GREEN);
         
     // Badan utama barrier
-    bar(x, y, x + 50, y + 30);
+    bar(x, y - 5, x + 80, y + 25);
         
-    // Membuat lengkungan dengan blok bertingkat
-    bar(x + 5, y - 5, x + 45, y);
-    bar(x + 10, y - 10, x + 40, y - 5);
-    bar(x + 15, y - 15, x + 35, y - 10);
-    bar(x + 20, y - 20, x + 30, y - 15);
+    // Membuat lengkungan dengan blok bertingkat lebih besar
+    bar(x + 5, y - 10, x + 75, y - 5);
+    bar(x + 10, y - 15, x + 70, y - 10);
+    bar(x + 15, y - 20, x + 65, y - 15);
+  
         
-    // Potongan di tengah bawah
+    // Potongan di tengah bawah lebih besar
     setfillstyle(SOLID_FILL, BLACK);
-    bar(x + 15, y + 20, x + 35, y + 30);
+    bar(x + 15, y + 15, x + 65, y + 25);
+    bar(x + 20, y + 10, x + 60, y + 20);
+    bar(x + 25, y + 5, x + 55, y + 15);
+
 }
 
 void barBarrier(){

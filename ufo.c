300457905212229cf3a1f0dd747ec3707f7bac93
Dutlesson <fiandraputeramardani@gmail.com>
2025-03-13 13:04:15 +0700

    sudah ada bar hp

--- ufo.c
+++ ufo.c
@@ -3,37 +3,72 @@
 #include "ufo.h"
 #include "score.h"
 #include "alien.h"
-
+#include <stdlib.h>
 
 // Inisialisasi UFO
 float ufoX = 100.0, ufoY = 100.0;
 float ufoSpeed = 2.5;
 int ufoDirection = 1;    
-int page = 0; // Untuk double buffering
 int ufoActive = 1;
-int ufoHealth;  // Tambahkan variabel untuk nyawa/hit UFO
+int ufoHealth = 15 + rand() % 6; // HP antara 15 - 20
+const int ufoMaxHealth = 20;
 
-// Inisialisasi peluru UFO
-int ufoBulletX = -1, ufoBulletY = -1, ufoBulletActive = 0;
-
-Alien aliens[MAX_ALIENS];
-
-// **Tambahkan fungsi untuk reset UFO dengan HP random**
-void resetUFO();
+// Inisialisasi peluru UFO menggunakan ADT AlienBullet
+AlienBullet ufoBullets[MAX_ALIEN_BULLETS];
+int ufoShootCooldown = 0;
 
 void resetUFO() {
-    ufoX = 100.0; // Reset posisi horizontal aja
+    ufoX = 100.0;
     ufoSpeed = 3.0;
     ufoDirection = 1;
-    ufoHealth = 15 + rand() % 6;
+    ufoHealth = 15 + rand() % 6; // HP antara 15 - 20
     ufoActive = 1;
+    for (int i = 0; i < MAX_ALIEN_BULLETS; i++) {
+        ufoBullets[i].active = 0;
+    }
+}
+
+void shootUFOBullet() {
+    if (ufoShootCooldown > 0) {
+        ufoShootCooldown--;
+        return;
+    }
+    
+    for (int i = 0; i < MAX_ALIEN_BULLETS; i++) {
+        if (!ufoBullets[i].active) {
+            ufoBullets[i].x = ufoX;
+            ufoBullets[i].y = ufoY + 20;
+            ufoBullets[i].active = 1;
+            ufoShootCooldown = 50; // Delay antara tembakan
+            break;
+        }
+    }
+}
+
+void updateUFOBullets() {
+    for (int i = 0; i < MAX_ALIEN_BULLETS; i++) {
+        if (ufoBullets[i].active) {
+            ufoBullets[i].y += 5;
+            if (ufoBullets[i].y > getmaxy()) {
+                ufoBullets[i].active = 0;
+            }
+        }
+    }
+}
+
+void drawUFOBullets() {
+    setcolor(RED);
+    for (int i = 0; i < MAX_ALIEN_BULLETS; i++) {
+        if (ufoBullets[i].active) {
+            bar(ufoBullets[i].x - 2, ufoBullets[i].y, ufoBullets[i].x + 2, ufoBullets[i].y + 10);
+        }
+    }
 }
 
 void drawUFO(int x, int y) {
-    // Geser ke atas lebih jauh, dari -20 jadi -40
     y -= 40;
-
-    // Body utama UFO (ukuran tetap seperti sebelumnya)
+    
+    // Body utama UFO
     setcolor(RED);
     setfillstyle(SOLID_FILL, RED);
     fillellipse(x, y, 60, 25);
@@ -67,7 +102,7 @@ void drawUFO(int x, int y) {
         fillellipse(x + i - 1, y + 13, 1, 1);
     }
 
-    // Kaki
+    // Kaki UFO
     setcolor(DARKGRAY);
     setlinestyle(SOLID_LINE, 0, 3);
     line(x - 45, y + 20, x - 55, y + 45);
@@ -76,71 +111,47 @@ void drawUFO(int x, int y) {
     rectangle(x + 50, y + 45, x + 55, y + 50);
     floodfill(x - 52, y + 47, DARKGRAY);
     floodfill(x + 52, y + 47, DARKGRAY);
-}    
-
-void drawBullet(int bx, int by) {
-    setcolor(YELLOW);
-    setfillstyle(SOLID_FILL, YELLOW);
-    fillellipse(bx, by, 3, 3);
+    
+    // Health bar di atas UFO
+    int barWidth = 50;
+    int barHeight = 5;
+    int barX = x - barWidth / 2;
+    int barY = y - 50;
+    
+    setcolor(WHITE);
+    rectangle(barX, barY, barX + barWidth, barY + barHeight);
+    
+    int hpWidth = (ufoHealth * barWidth) / ufoMaxHealth;
+    setfillstyle(SOLID_FILL, GREEN);
+    bar(barX, barY, barX + hpWidth, barY + barHeight);
 }
 
 void UFO(Alien aliens[]) {
-    if (!ufoActive) return; // Jika UFO sudah mati, tidak usah update
+    if (!ufoActive) return;
 
-    // Update posisi UFO
     ufoX += ufoDirection * ufoSpeed;
     if (ufoX > getmaxx() - 60 || ufoX < 60) ufoDirection *= -1;
+    
     drawUFO((int)ufoX, (int)ufoY);
+    shootUFOBullet();
+    updateUFOBullets();
+    drawUFOBullets();
 
-    // Cek jika UFO terkena tembakan
     for (int j = 0; j < MAX_BULLETS; j++) {
         if (bullets_player[j].active &&
             bullets_player[j].x > ufoX - 45 &&
             bullets_player[j].x < ufoX + 45 &&
-            bullets_player[j].y > ufoY - 40 && // Dari -20 jadi -40
+            bullets_player[j].y > ufoY - 40 && 
             bullets_player[j].y < ufoY + 20) {
-
-            bullets_player[j].active = 0; // Nonaktifkan peluru
-            ufoHealth--; // Kurangi nyawa UFO
-
-            if (ufoHealth <= 0) { // Jika nyawa habis, matikan UFO
+            
+            bullets_player[j].active = 0;
+            ufoHealth--;
+            
+            if (ufoHealth <= 0) {
                 ufoActive = 0;
-                addUFOScore(); // Tambah skor
-                
-                // Spawn ulang UFO setelah beberapa waktu
-                delay(10); // Bisa diatur ulang
-                resetUFO(); // Cuma reset X, health, dll, gak ubah Y
-            }
-        }
-    }
-
-    // UFO menembak
-    if (ufoBulletActive) {
-        ufoBulletY += 10;
-        drawBullet(ufoBulletX, ufoBulletY);
-        if (ufoBulletY > getmaxy()) ufoBulletActive = 0;
-    } else {
-        ufoBulletX = (int)ufoX;
-        ufoBulletY = (int)ufoY + 15; // Dari +20 jadi +15
-        ufoBulletActive = 1;
-    }
-
-    // Deteksi tabrakan dengan alien
-    for (int i = 0; i < MAX_ALIENS; i++) {
-        if (aliens[i].active) {
-            int ufoLeft = (int)ufoX - 60;
-            int ufoRight = (int)ufoX + 60;
-            int ufoTop = (int)ufoY - 25;
-            int ufoBottom = (int)ufoY + 25;
-
-            int alienLeft = aliens[i].x;
-            int alienRight = aliens[i].x + BLOCK_SIZE;
-            int alienTop = aliens[i].y;
-            int alienBottom = aliens[i].y + BLOCK_SIZE;
-
-            if (ufoRight > alienLeft && ufoLeft < alienRight &&
-                ufoBottom > alienTop && ufoTop < alienBottom) {
-                aliens[i].active = 0;
+                addUFOScore();
+                delay(10);
+                resetUFO();
             }
         }
     }

74e9ecda3d5e5506692d50a14c15d5e0258f2e9c
nazriel <nazrielramdhani5@gmail.com>
2025-05-12 11:11:50 +0700

    Pemindahan Modul-modul level#2

--- include/alien.h
+++ include/alien.h
@@ -2,11 +2,16 @@
 #define ALIEN_H
 #include <graphics.h>
 #include "mainsprite.h"
+#include "gameplay.h"
 
 // Constants
 #define ALIEN_ROWS 6
 #define ALIEN_COLS 10
 #define MAX_ALIEN_BULLETS 20
+#define BASE_ALIEN_SPEED 0.5  // Kecepatan dasar pergerakan alien (BLOCK_SIZE per frame)
+#define BASE_SHOOT_INTERVAL 5000  // Interval dasar untuk tembakan alien (semakin kecil, semakin cepat)
+#define SPEED_INCREMENT 0.1  // Peningkatan kecepatan per level
+#define SHOOT_INTERVAL_DECREMENT 500  // Pengurangan interval tembakan per level
 
 // Structure for an alien
 typedef struct Alien {
@@ -45,5 +50,14 @@ void initLevel();
 
 // Fungsi untuk memeriksa apakah semua alien habis dan menaikkan level
 void checkAndUpdateLevel(Alien aliens[ALIEN_ROWS][ALIEN_COLS]);
+
+// Fungsi untuk mendapatkan kecepatan alien berdasarkan level
+float getAlienSpeed();
+
+// Fungsi untuk mendapatkan interval tembakan alien berdasarkan level
+int getShootInterval();
+
+// Fungsi untuk mendapatkan level saat ini
+int getCurrentLevel();
 
 #endif // ALIEN_H
